43a8f26efc9de112be2f84ef418607a1e72c3e67
Lepha <lepharamchiary@gmail.com>
2024-08-09 10:33:21 +0530

    e-commerce order model

---
+++ models/e-commerse/order.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const productItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongooses.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems: {
+        type: [productItemSchema]
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING", "CANCELLED", "DELIVERD"],
+        default: "PENDING"
+    }
+}, {timestamps: true})
+
+export const Order = mongoose.model("Order", orderSchema)
